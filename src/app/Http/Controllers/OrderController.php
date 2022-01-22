9a98a1b61ef5a4785ff47e465b1d586c54b0a28a
worldscourage <pogosovworldscourage@gmail.com>
2022-01-22 19:54:01 +0200

    proper order request

--- src/app/Http/Controllers/OrderController.php
+++ src/app/Http/Controllers/OrderController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateOrderRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Validation\Rule;
+use services\Order\OrderCreator;
+use services\Order\OrderRequestDto;
 
 class OrderController extends Controller
 {
@@ -19,10 +23,16 @@ class OrderController extends Controller
         //
     }
 
-    public function create(Request $request)
+    public function create(CreateOrderRequest $request)
     {
-        $user = User::find(1);
-        echo "qwer" . $user->email; exit;
+        $user = User::find(1); //todo authentication
+        var_dump($request->country);
+        var_dump($request->products);
+        var_dump(json_encode([['cnd'=> 1, 'id' => 1]]));
+        var_dump(json_decode($request->products, 1));
+        exit;
+        //$dto = new OrderRequestDto(, $request->)
+        //$orderCreator = new OrderCreator($user, );
     }
 
 }
---
+++ src/app/Http/Requests/CreateOrderRequest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Class CreateOrderRequest
+ * @package App\Http\Requests
+ *
+ * @property $products
+ * @property string $country
+ */
+class CreateOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'products' => 'required|json',
+            'country' => 'required|exists:countries,id',
+        ];
+    }
+
+    /**
+     * Custom Failed Response
+     *
+     * Overrides the Illuminate\Foundation\Http\FormRequest
+     * response function to stop it from auto redirecting
+     * and applies a API custom response format.
+     *
+     * @param array $errors
+     * @return JsonResponse
+     */
+    public function response(array $errors) {
+        return new JsonResponse([
+            'status' => '422',
+            'errors' => $errors,
+        ], 422);
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()], 422));
+    }
+}
